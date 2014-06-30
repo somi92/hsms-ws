473c263ff324c1fdaacf623dd5f956babce6e80c
somi92 <milos92s@yahoo.com>
2014-06-30 20:49:07 +0200

    Updated database script.

--- hsms_data.sql
+++ hsms_data.sql
@@ -1,21 +1,22 @@
 create table ORGANIZACIJA (
 	
 	org_id numeric(7),
-	naziv varchar(255),
-	opis varchar(1024),
-	website varchar(255),
+	naziv varchar(255) character set utf8,
+	opis varchar(1024) character set utf8,
+	website varchar(255) character set utf8,
 	constraint ORG_PK primary key (org_id)
-);
+) DEFAULT CHARACTER SET utf8   
+ COLLATE utf8_unicode_ci;
 
 create table HUMANITARNI_BROJ (
 
 	hb_id numeric(7),
-	opis varchar(255),
-	broj varchar(30),
-	cena varchar(255),
-	hb_status varchar(10),
-	prioritet numeric(4),
-	napomena varchar(255),
+	opis varchar(255) character set utf8,
+	broj varchar(30) character set utf8,
+	cena varchar(255) character set utf8,
+	hb_status varchar(10) character set utf8,
+	prioritet numeric(4) ,
+	napomena varchar(255) character set utf8,
 	org_id numeric(7),
 
 	constraint HB_PK primary key (hb_id),
@@ -23,7 +24,8 @@ create table HUMANITARNI_BROJ (
 		references ORGANIZACIJA (org_id),
 	constraint HB_STATUS_CHECK check (hb_status in('Otvoren','Zatvoren','Nepoznato')),
 	constraint HB_PRIORITET_CHECK check (prioritet in(1,2,3))
-);
+) DEFAULT CHARACTER SET utf8   
+ COLLATE utf8_unicode_ci;
 
 
 drop table HUMANITARNI_BROJ;
@@ -44,7 +46,24 @@ insert into HUMANITARNI_BROJ values (204,'Humanitarna akcija "65+ Pokret za brig
 insert into HUMANITARNI_BROJ values (205,'Humanitarna akcija "Srce za decu"','2552','100 din','Otvoren',2,'-',103);
 insert into HUMANITARNI_BROJ values (206,'Humanitarna akcija "Jedan u milion"','2112','100 din','Otvoren',2,'-',104);
 insert into HUMANITARNI_BROJ values (207,'Humanitarna akcija za pomoć u lečenju Une Savić','2205','100 din','Otvoren',1,'-',101);
+insert into HUMANITARNI_BROJ values (208,'Humanitarna akcija za pomoć u lečenju Vuka Lojkovića','7553','100 din','Otvoren',1,'-',101);
 
 select HB.hb_id, HB.opis, HB.broj, HB.cena, ORG.naziv, ORG.website, HB.prioritet, HB.napomena
-from HUMANITARNI_BROJ HB join ORGANIZACIJA ORG using (org_id)
+from HUMANITARNI_BROJ HB join ORGANIZACIJA ORG on (HB.org_id = ORG.org_id)
 order by HB.prioritet;
+
+ALTER DATABASE hsms_database
+ CHARACTER SET utf8
+ DEFAULT CHARACTER SET utf8
+ COLLATE utf8_unicode_ci
+ DEFAULT COLLATE utf8_unicode_ci;
+
+ALTER TABLE HUMANITARNI_BROJ
+ DEFAULT CHARACTER SET utf8
+ COLLATE utf8_unicode_ci
+ ;
+
+ALTER TABLE ORGANIZACIJA
+ DEFAULT CHARACTER SET utf8
+ COLLATE utf8_unicode_ci
+ ;
